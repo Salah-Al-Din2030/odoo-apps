753a88cd3f5aca93b22c183ac6b73f258b2b2a01
Salah-Al-Din2030 <salahaldin2030@gmail.com>
2024-02-17 03:04:57 +0300

    remove module from main

--- pos_global_discount_in_line/models/pos_config.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-
-class PosConfig(models.Model):
-    _inherit = "pos.config"
-
-    global_discount_in_line = fields.Boolean(string="Add Discount in Line" ,default=True)
-    global_fixes_discount_in_line = fields.Boolean(string="Fixed amount Discount?" ,default=True)
